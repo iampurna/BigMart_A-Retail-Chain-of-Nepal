0663666ccacd98e9f9e4363dc5d8eb82f5860750
iampurna <linkwithpurna@gmail.com>
2024-09-24 17:31:42 +0545

    ng basics

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/header/header.component';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/modules/product';
+import { Pagination } from './shared/modules/pagination';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  baseUrl = 'https://localhost:5001/api/';
+  // constructor(private http: HttpClient) {}
+  private http = inject(HttpClient);
+
   title = 'Bigmart';
+  products: Product[] = [];
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
+      next: (response) => (this.products = response.data),
+      error: (error) => console.log(error),
+      complete: () => console.log('complete'),
+    });
+  }
 }

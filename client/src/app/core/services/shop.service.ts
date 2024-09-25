8a87b86c7789bef2afceff63d3962e1ed8d8daa7
iampurna <linkwithpurna@gmail.com>
2024-09-25 22:53:23 +0545

    fixes

--- client/src/app/core/services/shop.service.ts
+++ client/src/app/core/services/shop.service.ts
@@ -27,7 +27,7 @@ export class ShopService {
     }
 
     if (shopParams.search) {
-      params = params.append('sort', shopParams.search);
+      params = params.append('search', shopParams.search);
     }
     params = params.append('pageSize', shopParams.pageSize);
     params = params.append('pageIndex', shopParams.pageNumber);
